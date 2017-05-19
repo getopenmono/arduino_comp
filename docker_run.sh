5defefe5537063c46cab2af2167034b3be0bac55
Kristoffer Andersen <ka@ptype.dk>
2017-05-19 15:19:58 +0200

    Added version parameter to docker run

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,6 @@
-docker run -tie RELEASE_VERSION=1.6.7 -e DEPLOY_DIR=/Desktop -v $HOME/Desktop:/Desktop monolit/arduinobuild
+if [ $# -lt 1 ]; then
+	echo "Not enough args! Usage: docker_run.sh VERSION"
+	exit 1
+fi
+
+docker run -tie RELEASE_VERSION=$1 -e DEPLOY_DIR=/Desktop -v $HOME/Desktop:/Desktop monolit/arduinobuild
