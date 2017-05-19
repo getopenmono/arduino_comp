81d0ca62e8ebcd03bb50f301614578080158373d
Kristoffer Andersen <ka@ptype.dk>
2017-05-19 15:28:08 +0200

    docker run script can copy deploy files to custom target dir

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,13 @@
 if [ $# -lt 1 ]; then
-	echo "Not enough args! Usage: docker_run.sh VERSION"
+	echo "Not enough args! Usage: docker_run.sh VERSION [TARGET_DIR]"
 	exit 1
 fi
 
-docker run -tie RELEASE_VERSION=$1 -e DEPLOY_DIR=/Desktop -v $HOME/Desktop:/Desktop monolit/arduinobuild
+if [ $# -gt 1 ]; then
+	DEPLOY_TARGET=$2
+else
+	DEPLOY_TARGET="$HOME/Desktop"
+fi
+	
+echo "Deploy files are copied to $DEPLOY_TARGET"
+docker run -tie RELEASE_VERSION=$1 -e DEPLOY_DIR=/Desktop -v $DEPLOY_TARGET:/Desktop monolit/arduinobuild
